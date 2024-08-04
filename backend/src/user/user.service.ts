0a34d9d33eab4e5686d82c67c773c31c8590e357
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-04 11:52:26 +0900

    refactor: 불필요한 메소드 삭제

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -14,13 +13,5 @@ export class UserService {
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }

5ddfb6fe1abb928549b4a5ed540571d4d4de7e29
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-04 17:50:57 +0900

    feat: AppModule에 Module import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database.module';
+import { ImageModule } from './image/image.module';
+import { CategoryModule } from './category/category.module';
+import { ImageCategoryModule } from './image-category/image-category.module';
+import { UserImageHistoryModule } from './user-image-history/user-image-history.module';
+import { ImageReplyModule } from './image-reply/image-reply.module';
+import { SaveImageModule } from './save-image/save-image.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +17,13 @@ import { DatabaseModule } from './database.module';
       isGlobal: true,
     }),
     DatabaseModule,
+    UserModule,
+    ImageModule,
+    CategoryModule,
+    ImageCategoryModule,
+    UserImageHistoryModule,
+    ImageReplyModule,
+    SaveImageModule,
   ],
   controllers: [AppController],
   providers: [AppService],

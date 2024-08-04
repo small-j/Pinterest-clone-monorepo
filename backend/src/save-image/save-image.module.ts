30416b888c5884ac3fbb0e85e86e78494a4b1a14
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-04 22:19:43 +0900

    feat: save-image custom repository 생성

--- backend/src/save-image/save-image.module.ts
+++ backend/src/save-image/save-image.module.ts
@@ -3,10 +3,12 @@ import { SaveImageService } from './save-image.service';
 import { SaveImageController } from './save-image.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SaveImage } from './entities/save-image.entity';
+import { SaveImageRepository } from './save-image.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SaveImage])],
   controllers: [SaveImageController],
-  providers: [SaveImageService],
+  providers: [SaveImageService, SaveImageRepository],
+  exports: [SaveImageRepository],
 })
 export class SaveImageModule {}

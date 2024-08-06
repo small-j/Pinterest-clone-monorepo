6ad4c62457ae25dfc8b6068f11d4623c6ff6589e
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-07 01:32:06 +0900

    refactor: save-image repository 사용하지 않는 메소드 삭제

--- backend/src/save-image/save-image.repository.ts
+++ backend/src/save-image/save-image.repository.ts
@@ -1,42 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SaveImage } from './entities/save-image.entity';
-import { User } from 'src/user/entities/user.entity';
 import { Image } from 'src/image/entities/image.entity';
 
 @Injectable()
-export class SaveImageRepository {
-  constructor(
-    @InjectRepository(SaveImage)
-    private readonly saveImageRepository: Repository<SaveImage>,
-  ) {}
-
-  async addSaveImage(saveImage: SaveImage): Promise<void> {
-    await this.saveImageRepository.save(saveImage);
-  }
-
-  async findById(id: number): Promise<SaveImage> {
-    return this.saveImageRepository.findOneBy({ id: id });
-  }
-
-  async findByUserIdAndImageId(user: User, image: Image): Promise<SaveImage> {
-    return this.saveImageRepository.findOne({ where: { user, image } });
-  }
-
-  async deleteSaveImage(id: number): Promise<void> {
-    await this.saveImageRepository.delete(id);
-  }
-
+export class SaveImageRepository extends Repository<SaveImage> {
   async deleteSaveImageToImage(image: Image): Promise<void> {
-    await this.saveImageRepository.delete({ image });
+    await this.delete({ image });
   }
 
   async findByImage(image: Image): Promise<SaveImage[]> {
-    return this.saveImageRepository.find({ where: { image } });
+    return this.find({ where: { image } });
   }
 
-  async findByUser(user: User): Promise<SaveImage[]> {
-    return this.saveImageRepository.find({ where: { user } });
-  }
+  // TODO: 아래 삭제.
+  // async findByUser(user: User): Promise<SaveImage[]> {
+  //   return this.find({ where: { user: user } });
+  // }
+
+  // async findByUser(userId: number): Promise<SaveImage[]> {
+  //   return this.createQueryBuilder('saveImage')
+  //     .innerJoinAndSelect('saveImage.user', 'user')
+  //     .where('user.id = :userId', { userId })
+  //     .getMany();
+  // }
 }

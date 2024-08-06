a8fd82f2a46188cd0cef531e538d4212cdcd5a9e
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-07 01:20:05 +0900

    feat: image-category service, controller 삭제

--- backend/src/image-category/image-category.module.ts
+++ backend/src/image-category/image-category.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ImageCategoryService } from './image-category.service';
-import { ImageCategoryController } from './image-category.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImageCategory } from './entities/image-category.entity';
 import { ImageCategoryRepository } from './image-category.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ImageCategory])],
-  controllers: [ImageCategoryController],
-  providers: [ImageCategoryService, ImageCategoryRepository],
+  providers: [ImageCategoryRepository],
   exports: [ImageCategoryRepository],
 })
 export class ImageCategoryModule {}
--- backend/src/image-category/image-category.repository.ts
+++ backend/src/image-category/image-category.repository.ts
@@ -1,27 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ImageCategory } from './entities/image-category.entity';
 import { Image } from 'src/image/entities/image.entity';
 
 @Injectable()
-export class ImageCategoryRepository {
-  constructor(
-    @InjectRepository(ImageCategory)
-    private readonly repository: Repository<ImageCategory>,
-  ) {}
-
+export class ImageCategoryRepository extends Repository<ImageCategory> {
   async addImageCategory(imageCategory: ImageCategory): Promise<void> {
-    await this.repository.save(imageCategory);
+    await this.save(imageCategory);
   }
 
   async findByImage(image: Image): Promise<ImageCategory[]> {
-    return this.repository.find({ where: { image } });
+    return this.find({ where: { image } });
   }
 
   async getCategoryFromSearchWord(searchStr: string): Promise<ImageCategory[]> {
-    return this.repository
-      .createQueryBuilder('ic')
+    return this.createQueryBuilder('ic')
       .innerJoinAndSelect('ic.category', 'c')
       .where('c.name LIKE :searchStr', { searchStr: `%${searchStr}%` })
       .getMany();

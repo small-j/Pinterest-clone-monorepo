2feffc2a72c82f84796d3482b1088c8539760aad
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-08 23:38:20 +0900

    feat: image controller 마이그레이션

--- backend/src/image/image.controller.ts
+++ backend/src/image/image.controller.ts
@@ -1,28 +1,63 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Body,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ImageService } from './image.service';
 import { CreateImageDto } from './dto/create-image.dto';
+import { GetUploadImageDataDto } from './dto/get-upload-image-data.dto';
+import { CreateMetaImageDto } from './dto/create-meta-image.dto';
+import { GetImageDetailDto } from './dto/get-image-detail.dto';
+import { GetImageDto } from './dto/get-image.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
   @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imageService.create(createImageDto);
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadImage(
+    @UploadedFile() req: Express.Multer.File,
+  ): Promise<GetUploadImageDataDto> {
+    const imageRequest = new CreateImageDto(req);
+    return await this.imageService.uploadImage(imageRequest);
+  }
+
+  @Post('/meta')
+  async addImage(
+    @Body() imageMetaRequest: CreateMetaImageDto,
+  ): Promise<number> {
+    return await this.imageService.addImage(imageMetaRequest);
+  }
+
+  @Delete()
+  async deleteImage(@Query('id') id: number): Promise<number> {
+    return await this.imageService.deleteImage(id);
   }
 
   @Get()
-  findAll() {
-    return this.imageService.findAll();
+  async findImage(
+    @Query('id') id: number,
+    @Query('user_id') userId: number = -1,
+  ): Promise<GetImageDetailDto> {
+    return await this.imageService.findImage(id, userId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.imageService.findOne(+id);
+  @Get('/search')
+  async searchImage(
+    @Query('search-word') searchWord: string,
+  ): Promise<GetImageDto[]> {
+    return await this.imageService.getSearchImages(searchWord);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imageService.remove(+id);
+  @Get('/main')
+  async mainImage(@Query('user-id') id: number): Promise<GetImageDto[]> {
+    return await this.imageService.getMainImages(id);
   }
 }

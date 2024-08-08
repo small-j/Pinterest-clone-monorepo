5d0e51334d657b2327d4b4439e884dd596a409b4
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-08 23:35:37 +0900

    feat: user controller 마이그레이션

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,23 +1,39 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Query,
+  HttpStatus,
+  HttpCode,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginInfoDto } from './dto/login-info.dto';
+import { UserInfoeDto } from './dto/user-info.dto';
+import { User } from './entities/user.entity';
+import { JwtTokenInterceptor } from 'src/interceptor/jwt-token.interceptor';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Post('/join')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() userRequest: CreateUserDto): Promise<number> {
+    return this.userService.register(userRequest);
+  }
+
+  @Post('/login')
+  @HttpCode(HttpStatus.OK)
+  @UseInterceptors(JwtTokenInterceptor)
+  async login(@Body() loginInfoRequest: LoginInfoDto): Promise<User> {
+    return await this.userService.login(loginInfoRequest);
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  async getUserInfo(@Query('id') id: number): Promise<UserInfoeDto> {
+    return this.userService.getUserInfo(id);
   }
 }

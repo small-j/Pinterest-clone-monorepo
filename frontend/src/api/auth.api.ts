1343c553434e5d598387db4e6c8efc1a4d595f5c
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-15 18:02:11 +0900

    feat: join API 연결

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -1,31 +1,63 @@
-import { commonValue } from "./common.value";
-import { ErrorResponse, Response } from "./types/common.data.type";
+import { commonValue } from './common.value';
+import { ErrorResponse, Response } from './types/common.data.type';
 
-const PREFIX_URL = "/user"
+const PREFIX_URL = '/user';
 
 interface LoginInfo {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
-export async function login(loginInfo: LoginInfo, callback: (data: Response<string> | ErrorResponse) => void) {
-    try {
-        const result = await fetch(`${commonValue.ORIGIN}${PREFIX_URL}/login`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: loginInfo.email,
-                password: loginInfo.password,
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        }).then(res =>{
-            if (!res.headers.get(commonValue.TOKEN_HEADER)) throw new Error();
-            commonValue.ACCESS_TOKEN = res.headers.get(commonValue.TOKEN_HEADER) as string;
+interface JoinInfo {
+    name: string;
+    email: string;
+    password: string;
+  }
 
-            return res.json();
-        });
+export async function login(
+  loginInfo: LoginInfo,
+  callback: (data: Response<string> | ErrorResponse) => void,
+) {
+  try {
+    const result = await fetch(`${commonValue.ORIGIN}${PREFIX_URL}/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: loginInfo.email,
+        password: loginInfo.password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => {
+      if (!res.headers.get(commonValue.TOKEN_HEADER)) throw new Error();
+      commonValue.ACCESS_TOKEN = res.headers.get(
+        commonValue.TOKEN_HEADER,
+      ) as string;
 
-        return callback({ data: result,  success: true });
-    } catch {
-        callback({ data: null, errorMessage: "로그인 실패", success: false });
-    }
+      return res.json();
+    });
+
+    return callback({ data: result, success: true });
+  } catch {
+    callback({ data: null, errorMessage: '로그인 실패', success: false });
+  }
+}
+
+export async function join(
+  joinInfo: JoinInfo,
+  callback: (data: Response<string> | ErrorResponse) => void,
+) {
+  try {
+    const result = await fetch(`${commonValue.ORIGIN}${PREFIX_URL}/join`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: joinInfo.name,
+        email: joinInfo.email,
+        password: joinInfo.password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => res.json());
+
+    return callback({ data: result, success: true });
+  } catch {
+    callback({ data: null, errorMessage: 'Failed to sign up', success: false });
+  }
 }

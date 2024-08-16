22e2e5c09a1e638aa2ed0757ff4cc0336e718dd1
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-16 23:34:01 +0900

    feat: search page 생성

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,6 +1,36 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { getSearchImages } from '../api/image.api';
+import ImagePinList from '../components/image/ImagePinList';
+import { ImagePins, SearchImage } from '../api/types/image.data.type';
+import { ErrorResponse, Response, ResponseCallback } from '../api/types/common.data.type';
+import { useEffect, useState } from 'react';
+
 function SearchPage() {
+  const param = useParams();
+  const [imageDatas, setImageDatas] = useState<
+    Response<ImagePins> | ErrorResponse
+  >();
+
+  const requestSearchImages = (searchWord: string) => {
+    if (!searchWord || searchWord === '') useNavigate()('/');
+    const callback: ResponseCallback<SearchImage> = (res) => {
+      setImageDatas(res);
+    }
+
+    getSearchImages(searchWord, callback);
+  };
+
+  useEffect(() => {
+    requestSearchImages(param.searchWord || '');
+  }, []);
+  useEffect(() => {
+    requestSearchImages(param.searchWord || '');
+  }, [param.searchWord]);
+
   return (
-   <div></div>
+    <div>
+      <ImagePinList imageDatas={imageDatas}></ImagePinList>
+    </div>
   );
 }
 export default SearchPage;

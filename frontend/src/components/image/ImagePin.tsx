8ad2a9bd9a38caf1f80011af3cb7afab1745607a
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-19 02:19:09 +0900

    refactor: image pin UI 컴포넌트 공백 변경

--- frontend/src/components/image/ImagePin.tsx
+++ frontend/src/components/image/ImagePin.tsx
@@ -6,9 +6,9 @@ interface Props {
 function ImagePin({ id, url }: Props) {
   return (
     <a href={`/image-pin-detail?id=${id}`}>
-        <div className="border rounded-lg w-[236px] h-[400px] flex items-center justify-center m-2">
-            <img className="max-w-full max-h-full" src={url} alt="" />
-        </div>
+      <div className="border rounded-lg w-[236px] h-[400px] flex items-center justify-center m-2">
+        <img className="max-w-full max-h-full" src={url} alt="" />
+      </div>
     </a>
   );
 }

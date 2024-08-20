97391cf58e24842281562e4ffbd3dc57a6fa6dee
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-20 19:38:18 +0900

    feat: image 삭제 기능 추가

--- frontend/src/components/image/ImagePinDetail.tsx
+++ frontend/src/components/image/ImagePinDetail.tsx
@@ -1,13 +1,21 @@
 import { FieldValues, useForm } from 'react-hook-form';
 import Profile from '../common/Profile';
 import { Button } from '../shadcn/ui/button';
-import { Card, CardContent, CardFooter } from '../shadcn/ui/card';
+import { Card, CardContent, CardFooter, CardHeader } from '../shadcn/ui/card';
 import { Input } from '../shadcn/ui/input';
 import RepliesArccordion from './RepliesArccordion';
 import { createImageReply } from '../../api/reply.api';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { ImageReplyInfo } from '../../api/types/reply.data.type';
-import { HiPaperAirplane } from "react-icons/hi";
+import { HiPaperAirplane } from 'react-icons/hi';
+import { UserContext, UserContextValue } from '../../context/UserContext';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../shadcn/ui/dropdown-menu';
+import { CiMenuKebab } from 'react-icons/ci';
 
 interface Props {
   id: number;
@@ -18,6 +26,7 @@ interface Props {
   userName: string;
   userEmail: string;
   replies: ImageReplyInfo[];
+  deleteImagePinRequest: (id: number) => void;
 }
 
 function ImagePinDetail({
@@ -29,9 +38,11 @@ function ImagePinDetail({
   userName,
   userEmail,
   replies,
+  deleteImagePinRequest,
 }: Props) {
   const { register, handleSubmit } = useForm();
   const [repliesState, setRepliesState] = useState<ImageReplyInfo[]>(replies);
+  const { user } = useContext(UserContext) as UserContextValue;
 
   const addReply = (data: FieldValues) => {
     createImageReply({ content: data.reply, imageId: id }, (res) => {
@@ -40,14 +51,18 @@ function ImagePinDetail({
       setRepliesState([newReply, ...repliesState]);
     });
   };
-  
+
   const checkUserAthorization = () => {
-    // TODO: 삭제 권한이 있는 유저인지 확인.
-    return true;
+    return user && user.id === userId;
   };
 
   const handleDelete = () => {
-    // TODO 삭제 요청.
+    if (!checkUserAthorization()) return;
+    deleteImagePinRequest(id);
+  }
+
+  const deleteReplyInState = (id: number) => {
+    setRepliesState([...repliesState.filter((reply) => reply.id !== id)]);
   };
 
   return (
@@ -57,25 +72,44 @@ function ImagePinDetail({
       </div>
       <div className="w-2/4 h-full grow flex">
         <Card className="border-none shadow-none w-full h-full flex flex-col">
-          <div className="flex flex-row justify-end items-center pt-8 pr-8 sticky top-0 bg-white">
+          <CardHeader className="flex flex-row justify-between items-center pb-0 sticky top-0 bg-white">
             {checkUserAthorization() && (
-              <Button onClick={handleDelete}>삭제</Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <button className="rotate-90 w-8 h-8 flex items-center justify-center">
+                    <CiMenuKebab />
+                  </button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem onClick={handleDelete}>
+                    삭제
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             )}
             <Button className="m-0">저장</Button>
-          </div>
+          </CardHeader>
           <CardContent className="mt-8 grow">
             <h1 className="text-[28px] font-semibold">{title}</h1>
             <div className="text-base mt-4 mb-4">{content}</div>
             <Profile name={userName} email={userEmail}></Profile>
-            <RepliesArccordion replies={repliesState}></RepliesArccordion>
+            <RepliesArccordion
+              replies={repliesState}
+              deleteReplyInState={deleteReplyInState}
+            ></RepliesArccordion>
           </CardContent>
           <CardFooter className="sticky bottom-0 bg-white">
-            <form className='flex w-full'>
+            <form className="flex w-full">
               <Input
                 placeholder="댓글 추가"
                 {...register('reply', { required: true })}
               ></Input>
-              <button className='w-8 h-8 rotate-90 ml-2 flex justify-center items-center' onClick={handleSubmit((data) => addReply(data))}><HiPaperAirplane /></button>
+              <button
+                className="w-8 h-8 rotate-90 ml-2 flex justify-center items-center"
+                onClick={handleSubmit((data) => addReply(data))}
+              >
+                <HiPaperAirplane />
+              </button>
             </form>
           </CardFooter>
         </Card>
--- frontend/src/pages/ImagePinDetailPage.tsx
+++ frontend/src/pages/ImagePinDetailPage.tsx
@@ -1,45 +1,65 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import ImagePinDetail from '../components/image/ImagePinDetail';
 import { useEffect, useState } from 'react';
-import { getImageDetails } from '../api/image.api';
+import { deleteImagePin, getImageDetails } from '../api/image.api';
 import { ErrorResponse, Response } from '../api/types/common.data.type';
 import { ImageDetailsInfo } from '../api/types/image.data.type';
+import Loading from '../components/common/Loading';
 
 function ImagePinDetailPage() {
   const param = useParams();
   const navigate = useNavigate();
-  const [imageDetails, setImageDetails] = useState<Response<ImageDetailsInfo> | ErrorResponse>();
+  const [imageDetails, setImageDetails] = useState<
+    Response<ImageDetailsInfo> | ErrorResponse
+  >();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const isErrorResponse = (
     response: Response<ImageDetailsInfo> | ErrorResponse | undefined,
   ): response is ErrorResponse => {
     return !!response && response.success === false;
   };
-  
+
   useEffect(() => {
     checkParamValidation();
     getImageDeatilsData();
   }, [param.id]);
-  
+
   const checkParamValidation = () => {
     if (!param.id) navigate('/');
     return false;
-  }
+  };
 
   const getImageDeatilsData = () => {
     if (!param.id) return;
     getImageDetails(param.id, (res) => {
       setImageDetails(res);
     });
-  }
+  };
+
+  const deleteImagePinRequest = (id: number) => {
+    setLoading(true);
+    deleteImagePin(id.toString(), (res) => {
+      if (!res || !res.success) setLoading(false);
+      else if (res.success) navigate('/');
+    });
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center'>
+    <div className="flex flex-col items-center justify-center relative">
+      {loading && (
+        <Loading>
+          <div>loading...</div>
+        </Loading>
+      )}
       {!imageDetails && <div>loading...</div>}
       {isErrorResponse(imageDetails) && <div>{imageDetails.errorMessage}</div>}
-      {imageDetails &&
-        imageDetails.data?.imageDetails &&
-      <ImagePinDetail {...imageDetails.data.imageDetails}></ImagePinDetail>}
+      {imageDetails && imageDetails.data?.imageDetails && (
+        <ImagePinDetail
+          {...imageDetails.data.imageDetails}
+          deleteImagePinRequest={deleteImagePinRequest}
+        ></ImagePinDetail>
+      )}
     </div>
   );
 }

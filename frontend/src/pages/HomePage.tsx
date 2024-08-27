712a29cf298d1b93784bc397f88fcd56f8473466
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-27 23:34:06 +0900

    feat: image pin list에서 무한 스크롤 기능 지원이 추가됨에 따라 main image data에 추가된 pagination 데이터에 맞게 코드 변경

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,32 +1,53 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { getMainImages } from '../api/image.api';
 import ImagePinList from '../components/image/ImagePinList';
 import { ErrorResponse, Response } from '../api/types/common.data.type';
-import { ImagePins } from '../api/types/image.data.type';
+import { MainImage } from '../api/types/image.data.type';
+import { commonValue } from '../common.value';
 
 function HomePage() {
   const [imageDatas, setImageDatas] = useState<
-    Response<ImagePins> | ErrorResponse
+    Response<MainImage> | ErrorResponse
   >();
+  const page = useRef<number>(1);
 
   useEffect(() => {
-    getMainImages((res) => {
-      setImageDatas(res);
-    });
+    fetchData();
   }, []);
 
+  const fetchData = useCallback(() => {
+    if (!visualViewport) return;
+    const size = commonValue.IMAGE_DATA_PAGE_SIZE(visualViewport); 
+    const callback = (res: Response<MainImage> | ErrorResponse) => {
+      setImageDatas(res);
+      page.current = page.current + 1;
+    };
+    const seed = imageDatas?.data?.seed;
+
+    if (seed) getMainImages({ size, page: page.current }, callback, seed);
+    else getMainImages({ size, page: page.current }, callback);
+  }, [imageDatas]);
+
   const isErrorResponse = (
-    response: Response<ImagePins> | ErrorResponse | undefined,
+    response: Response<MainImage> | ErrorResponse | undefined,
   ): response is ErrorResponse => {
     return !!response && response.success === false;
   };
+
+  const isCanFetchData = useCallback(() => {
+    return !imageDatas?.data?.paginationInfo.isLastPage;
+  }, [imageDatas]);
 
   return (
     <div className="w-full flex flex-row flex-wrap justify-center">
       {!imageDatas && <div>loading...</div>}
       {isErrorResponse(imageDatas) && <div>{imageDatas.errorMessage}</div>}
       {imageDatas && imageDatas.success && imageDatas.data && (
-        <ImagePinList imageDatas={imageDatas.data}></ImagePinList>
+        <ImagePinList
+          imageDatas={imageDatas.data.imagePins}
+          fetchData={fetchData}
+          isCanFetchData={isCanFetchData}
+        ></ImagePinList>
       )}
     </div>
   );

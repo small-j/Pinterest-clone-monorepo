f4e072b8c47c43b4b1e543fd78aba39ea56d272b
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-27 23:33:27 +0900

    feat: image pin list 스크롤 시 추가 데이터 가져오는 무한 스크롤 기능 추가

--- frontend/src/components/image/ImagePinList.tsx
+++ frontend/src/components/image/ImagePinList.tsx
@@ -1,22 +1,65 @@
 import ImagePin from './ImagePin';
 import { ImagePins } from '@/src/api/types/image.data.type';
 import EmptyImagePin from './EmptyImagePin';
+import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 
 interface Props {
   imageDatas: ImagePins;
+  fetchData: () => void;
+  isCanFetchData: () => boolean;
 }
 
-function ImagePinList({ imageDatas }: Props) {
+function ImagePinList({ imageDatas, fetchData, isCanFetchData }: Props) {
+  const [isIntersect, setIsIntersect] = useState<boolean>(false);
+  const [images, setImages] = useState<ReactNode[]>([]);
+  const imagePinRef = useRef<HTMLDivElement | null>(null);
+
+  const callback: IntersectionObserverCallback = useCallback((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) setIsIntersect(true);
+    });
+  }, []);
+  const options: IntersectionObserverInit = {
+    rootMargin: '0px',
+    threshold: 0.5,
+  };
+
+  useEffect(() => {
+    if (!imageDatas) return;
+    const newImages = imageDatas.images?.map((value, index) => {
+      if (!value) return <EmptyImagePin key={index} />;
+
+      const { id, url } = value;
+      if (!id || !url) return <EmptyImagePin key={index} />;
+
+      return <ImagePin key={id} id={id} url={url}></ImagePin>;
+    });
+
+    setImages([...images, ...newImages]);
+  }, [imageDatas]);
+
+  useEffect(() => {
+    return () => setImages([]);
+  }, []);
+
+  useEffect(() => {
+    if (!imagePinRef.current) return;
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(imagePinRef.current);
+    return () => observer.disconnect();
+  }, [callback, imagePinRef]);
+
+  useEffect(() => {
+    if (isIntersect && isCanFetchData()) {
+      fetchData();
+      setIsIntersect(false);
+    }
+  }, [isIntersect]);
+
   return (
     <>
-      {imageDatas.images?.map((value, index) => {
-        if (!value) return <EmptyImagePin key={index} />;
-
-        const { id, url } = value;
-        if (!id || !url) return <EmptyImagePin key={index} />;
-
-        return <ImagePin key={id} id={id} url={url}></ImagePin>;
-      })}
+      {images}
+      <div ref={imagePinRef}></div>
     </>
   );
 }

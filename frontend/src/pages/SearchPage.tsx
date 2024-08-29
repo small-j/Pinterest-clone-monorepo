97fefe6d16f920bce109452f6df57f5ea5d668b4
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-30 00:41:51 +0900

    feat: search image data react-query 적용

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,31 +1,31 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import { getSearchImages } from '../api/image.api';
+import { getSearchImages, searchImageDataAdaptor } from '../api/image.api';
 import ImagePinList from '../components/image/ImagePinList';
 import { SearchImage } from '../api/types/image.data.type';
-import {
-  ErrorResponse,
-  Response,
-} from '../api/types/common.data.type';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { commonValue } from '../common.value';
+import { useQuery } from '@tanstack/react-query';
+import { validateSearchImageInfo } from '../validator/image.validator';
 
 function SearchPage() {
   const param = useParams();
-  const [imageDatas, setImageDatas] = useState<
-    Response<SearchImage> | ErrorResponse
-  >();
+  const [imageDatas, setImageDatas] = useState<SearchImage>();
   const page = useRef<number>(1);
   const [isFetching, setFetching] = useState(false);
   const isLastPage = useMemo<boolean>(() => {
-    if (!imageDatas || !imageDatas.data) return true;
-    return imageDatas.data.paginationInfo.isLastPage;
+    if (!imageDatas) return true;
+    return imageDatas.paginationInfo.isLastPage;
   }, [imageDatas]);
 
   useEffect(() => {
     setImageDatas(undefined); // 초기화. 추후 ImagePinList 마운트 되도록.
     page.current = 1;
-    requestSearchImages();
   }, [param.searchWord]);
+
+  const fetchData = () => {
+    setFetching(true);
+    page.current = page.current + 1;
+  };
 
   const requestSearchImages = useCallback(() => {
     const searchWord = param.searchWord;
@@ -34,36 +34,44 @@ function SearchPage() {
       return;
     }
     if (!visualViewport) return;
-
     const size = commonValue.IMAGE_DATA_PAGE_SIZE(visualViewport);
-    const callback = (res: Response<SearchImage> | ErrorResponse) => {
-      setImageDatas(res);
-      page.current = page.current + 1;
+
+    return getSearchImages({ size, page: page.current }, searchWord);
+  }, [param.searchWord, page.current]);
+
+  const { isPending, isError, error, data, isSuccess} = useQuery({
+    queryKey: ['searchImages', param.searchWord, page.current],
+    queryFn: () => requestSearchImages(),
+    staleTime: 5 * 60 * 1000,
+  });
+
+  useEffect(() => {
+    if (!isPending) {
       setFetching(false);
-    };
+  
+      // data가 없는 경우는 이전 화면을 유지한다.
+      if (isSuccess && data) setImageDatas(searchImageDataAdaptor(data));
+    }
+  }, [data]);
 
-    getSearchImages({ size, page: page.current }, searchWord, callback);
-    setFetching(true);
-  }, [param.searchWord]);
-
-  const isErrorResponse = (
-    response: Response<SearchImage> | ErrorResponse | undefined,
-  ): response is ErrorResponse => {
-    return !!response && response.success === false;
-  };
+  const validateImagePins = useCallback((data: SearchImage): boolean => {
+    validateSearchImageInfo(data);
+    return data && data.imagePins ? true : false;
+  }, []);
 
   return (
     <div className="w-full flex flex-wrap justify-center">
-      {!imageDatas && <div>loading...</div>}
-      {isErrorResponse(imageDatas) && <div>{imageDatas.errorMessage}</div>}
-      {imageDatas?.success && imageDatas.data && (
+      {isPending ? <div>loading...</div> : null}
+      {isError ? <div>{error.message}</div> : null}
+      {(imageDatas && validateImagePins(imageDatas)) ? (
         <ImagePinList
-          imageDatas={imageDatas.data.imagePins}
-          fetchData={requestSearchImages}
+          imageDatas={imageDatas.imagePins}
+          fetchData={fetchData}
           isFetching={isFetching}
           isLastPage={isLastPage}
         ></ImagePinList>
-      )}
+      ) : <div>검색 결과가 없습니다.</div>
+      }
     </div>
   );
 }

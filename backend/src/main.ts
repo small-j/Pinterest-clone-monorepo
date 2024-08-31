6ae73ef5ffe5e6c38a40902ef54f514f406fdec2
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-08-31 22:12:52 +0900

    feat: https 설정 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthGuard } from './guard/auth-guard';
 import { JwtProviderService } from './common/jwt-provider.service';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(
+      '/etc/letsencrypt/live/www.common-main-server.kr/privkey.pem',
+    ),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/www.common-main-server.kr/fullchain.pem',
+    ),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({
     origin: true,
     credentials: true,
